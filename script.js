6ce8d54fbebe5d958047c39ad5cdbcfa34b9001c
Wei Tang <weitang@Weis-MBP.cable.rcn.com>
2022-03-11 19:19:19 -0500

    Gorillas Promo Code

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(".btn").mouseup(function(){
     $(this).blur();
 })
+
+console.log("Welcome my friend! Fuck!");
+console.log(this);
+
+$("#copy-promo-button").on("click", function copyToClipboard() {
+    var promoCode = document.querySelector("#promo-code");
+    console.log(promoCode.value);
+    navigator.clipboard.writeText(promoCode.value);
+    alert("Promo Code '" + promoCode.value + "' Copied");
+});
