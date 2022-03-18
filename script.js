89ef3de0f8ad7fd0378f20f499d296ade3bc2847
Wei Tang <weitang@Weis-MBP.cable.rcn.com>
2022-03-18 03:15:42 -0400

    20220318

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ $(".btn").mouseup(function(){
     $(this).blur();
 })
 
-console.log("Welcome my friend! Fuck!");
-console.log(this);
 
 $("#copy-promo-button").on("click", function copyToClipboard() {
     var promoCode = document.querySelector("#promo-code");
     console.log(promoCode.value);
     navigator.clipboard.writeText(promoCode.value);
     alert("Promo Code '" + promoCode.value + "' Copied");
+});
+// do not touch the code above
+
+console.log("Welcome my friend! Fuck!");
+
+$("#submitPassword").on("click", function validatePassword() {
+    var getPassword = document.querySelector("#inputPassword").value;
+    console.log(getPassword);
+    if (getPassword == "weitangdesign") {
+        console.log("Password correct!");
+        window.location.href = "index.html";
+    }
+    else {
+        console.log("Password Wrong!");
+        alert("Wrong password, try again!")
+    };
 });
