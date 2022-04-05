0e3105ec0ae6c5e85337ca19a3f28e83e4cc72e4
Wei Tang <weitang@Weis-MBP.cable.rcn.com>
2022-04-05 12:11:09 -0400

    20220405

--- script.js
+++ script.js
@@ -22,19 +22,26 @@ console.log("Welcome my friend!");
 $("#submitPassword").on("click", function validatePassword() {
     var getPassword = document.querySelector("#inputPassword").value;
     console.log(getPassword);
-    var A = "we"
-    var B = "itan"
-    var C = 100
+    var A = "we";
+    var B = "itan";
+    var C = 100;
     if (getPassword == A+B+"g"+(C+11)*6) {
         console.log("Password correct!");
         // window.location.href = "index.html";
         console.log("hahha");
-        window.location.href = "index.html";
+        document.querySelector("#loader").classList.remove("invisible");
+        // window.location.href = "index.html";
+        setTimeout(function() {
+            window.location.href = "index.html";
+        }, 2000);
     }
     else {
         console.log("Password Wrong!");
         document.querySelector("#inputPassword").classList.add("border-danger");
         document.querySelector("#passwordAlert").classList.remove("invisible");
+        setTimeout(function() {
+            document.querySelector("#inputPassword").classList.remove("border-danger");
+        }, 1000);
         setTimeout(function() {
             document.querySelector("#passwordAlert").classList.add("invisible");
         }, 1000);
