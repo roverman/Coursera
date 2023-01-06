a9b05e367ca842f97eb427517c4b5af1f9d577bf
Wei Tang <weitang@Weis-MBP.cable.rcn.com>
2023-01-05 21:00:18 -0500

    1st API for Currency

--- script-play.js
+++ script-play.js
@@ -44,27 +44,106 @@ $('#loader').hide();
 
 
 
+// $("#usd-button").on("click", function transCurrency() {
+//     console.log("working");
+//     var UsdAmount = document.getElementById("USD").value;
+//     var Percentage = document.getElementById("Perc").value;
+//     var JpyAmount = UsdAmount*147.695*Percentage/100;
+//     var RmbAmount = UsdAmount*7.24*Percentage/100;
+//     var PoundAmount = UsdAmount*0.88*Percentage/100;
+//     var EuroAmount = UsdAmount*1.01*Percentage/100;
+//     var NewUsdAmount = UsdAmount*Percentage/100;
+//     document.getElementById("NewUSD").placeholder = Number(NewUsdAmount.toFixed(0)).toLocaleString();
+//     document.getElementById("JPY").placeholder = Number(JpyAmount.toFixed(0)).toLocaleString();
+//     document.getElementById("RMB").placeholder = Number(RmbAmount.toFixed(0)).toLocaleString();
+//     document.getElementById("POUND").placeholder = Number(PoundAmount.toFixed(0)).toLocaleString();
+//     document.getElementById("EURO").placeholder = Number(EuroAmount.toFixed(0)).toLocaleString();
+//     });
+
+
+
+
+var rmbRequestURL = 'https://api.exchangerate.host/convert?from='+'USD' + '&to=' + 'CNY';
+var rmbRequest = new XMLHttpRequest();
+rmbRequest.open('GET', rmbRequestURL);
+rmbRequest.responseType = 'json';
+rmbRequest.send();
+
+rmbRequest.onload = function() {
+  var rmbResponse = rmbRequest.response;
+  rmbExchangeRate = rmbResponse.info.rate;
+  console.log(rmbExchangeRate);
+  document.getElementById("RMB").placeholder = Number(rmbExchangeRate.toFixed(2)).toLocaleString();
+}
+
+
+
+
+var jpyRequestURL = 'https://api.exchangerate.host/convert?from='+'USD' + '&to=' + 'JPY';
+var jpyRequest = new XMLHttpRequest();
+jpyRequest.open('GET', jpyRequestURL);
+jpyRequest.responseType = 'json';
+jpyRequest.send();
+
+jpyRequest.onload = function() {
+  var jpyResponse = jpyRequest.response;
+  jpyExchangeRate = jpyResponse.info.rate;
+  console.log(jpyExchangeRate);
+  document.getElementById("JPY").placeholder = Number(jpyExchangeRate.toFixed(2)).toLocaleString();
+}
+
+
+
+var gbpRequestURL = 'https://api.exchangerate.host/convert?from='+'USD' + '&to=' + 'GBP';
+var gbpRequest = new XMLHttpRequest();
+gbpRequest.open('GET', gbpRequestURL);
+gbpRequest.responseType = 'json';
+gbpRequest.send();
+
+gbpRequest.onload = function() {
+  var gbpResponse = gbpRequest.response;
+  gbpExchangeRate = gbpResponse.info.rate;
+  console.log(gbpExchangeRate);
+  document.getElementById("POUND").placeholder = Number(gbpExchangeRate.toFixed(2)).toLocaleString();
+}
+
+
+
+var eurRequestURL = 'https://api.exchangerate.host/convert?from='+'USD' + '&to=' + 'EUR';
+var eurRequest = new XMLHttpRequest();
+eurRequest.open('GET', eurRequestURL);
+eurRequest.responseType = 'json';
+eurRequest.send();
+
+eurRequest.onload = function() {
+  var eurResponse = eurRequest.response;
+  eurExchangeRate = eurResponse.info.rate;
+  console.log(eurExchangeRate);
+  document.getElementById("EURO").placeholder = Number(eurExchangeRate.toFixed(2)).toLocaleString();
+}
+
+
+
+
+
+
 $("#usd-button").on("click", function transCurrency() {
     console.log("working");
     var UsdAmount = document.getElementById("USD").value;
     var Percentage = document.getElementById("Perc").value;
-    var JpyAmount = UsdAmount*147.695*Percentage/100;
-    var RmbAmount = UsdAmount*7.24*Percentage/100;
-    var PoundAmount = UsdAmount*0.88*Percentage/100;
-    var EuroAmount = UsdAmount*1.01*Percentage/100;
     var NewUsdAmount = UsdAmount*Percentage/100;
+
+    var JpyAmount = UsdAmount*jpyExchangeRate*Percentage/100;
+    var RmbAmount = UsdAmount*rmbExchangeRate*Percentage/100;
+    var PoundAmount = UsdAmount*gbpExchangeRate*Percentage/100;
+    var EuroAmount = UsdAmount*eurExchangeRate*Percentage/100;
+
     document.getElementById("NewUSD").placeholder = Number(NewUsdAmount.toFixed(0)).toLocaleString();
     document.getElementById("JPY").placeholder = Number(JpyAmount.toFixed(0)).toLocaleString();
     document.getElementById("RMB").placeholder = Number(RmbAmount.toFixed(0)).toLocaleString();
     document.getElementById("POUND").placeholder = Number(PoundAmount.toFixed(0)).toLocaleString();
     document.getElementById("EURO").placeholder = Number(EuroAmount.toFixed(0)).toLocaleString();
     });
-
-
-
-
-
-
 
 
 
