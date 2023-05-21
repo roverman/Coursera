bd3e30b387cde2ef8daebe0707ba201766556331
Wei Tang <weitang@Weis-MBP.cable.rcn.com>
2023-05-21 18:48:17 -0400

    working rent js

--- rent.js
+++ rent.js
@@ -1,3 +1,5 @@
+// var myModal = new bootstrap.Modal(document.getElementById('fullscreenModal'));
+
 var listings = [
     {
     "title":"Newport绝佳河景1B1B",
@@ -5,8 +7,8 @@ var listings = [
     "neogitable":"Yes",
     "location":"Queens",
     "address":"310 Livington St, Newport, NJ",
-    "lat":"40.7271548",
-    "lng":"-74.0325778",
+    "lat":40.7271548,
+    "lng":-74.0325778,
     "startDate":"08/12/2023",
     "endDate":"08/19/2023",
     "minStay":"5",
@@ -19,18 +21,24 @@ var listings = [
     "rate":"50",
     "neogitable":"True",
     "location":"Queens",
+    "address":"310 Livington St, Newport, NJ",
+    "lat":40.765261,
+    "lng":-73.989881,
     "startDate":"08/12/2023",
     "endDate":"08/19/2023",
     "minStay":"5",
-    "description":"",
+    "description":"2b1b 厕所是公共的(在房间外面) 有客厅和厨房",
     "wechat":"cocolemon",
     "cell":"284-342-5346",
     },
     {
-    "title":"LIC绝佳河景房",
+    "title":"LIC绝佳交通房",
     "rate":"50",
     "neogitable":"True",
     "location":"Queens",
+    "address":"310 Livington St, Newport, NJ",
+    "lat":40.749225,
+    "lng":-73.936498,
     "startDate":"08/12/2023",
     "endDate":"08/19/2023",
     "minStay":"5",
@@ -43,6 +51,9 @@ var listings = [
     "rate":"50",
     "neogitable":"True",
     "location":"Queens",
+    "address":"310 Livington St, Newport, NJ",
+    "lat":40.765255,
+    "lng":-73.989877,
     "startDate":"08/12/2023",
     "endDate":"08/19/2023",
     "minStay":"5",
@@ -51,16 +62,45 @@ var listings = [
     "cell":"284-342-5346",
     },
     {
-    "Link":"https://chat.openai.com/chat",
+    "title":"Manhattan绝佳河景房222",
+    "rate":"50",
+    "neogitable":"True",
+    "location":"Queens",
+    "address":"310 Livington St, Newport, NJ",
+    "lat":40.765256,
+    "lng":-73.989878,
+    "startDate":"08/12/2023",
+    "endDate":"08/19/2023",
+    "minStay":"5",
+    "description":"",
+    "wechat":"cocolemon",
+    "cell":"284-342-5346",
     },
     {
-    "Link":"https://chat.openai.com/chat",
+    "title":"Manhattan绝佳河景房3333",
+    "rate":"50",
+    "neogitable":"True",
+    "location":"Queens",
+    "address":"310 Livington St, Newport, NJ",
+    "lat":40.766255,
+    "lng":-73.989579,
+    "startDate":"08/12/2023",
+    "endDate":"08/19/2023",
+    "minStay":"5",
+    "description":"",
+    "wechat":"cocolemon",
+    "cell":"284-342-5346",
     },
 ];
 
 
 $.each(listings, function(i){
-    var listingCardString = '<div class="card-wrap col-lg-4 col-md-12"><div class="card"><img class="card-img-top" src="https://media.architecturaldigest.com/photos/62bdb5fda47d113b36863416/16:9/w_1738,h_977,c_limit/Mod%20Tree%202.jpeg" alt="Card image cap"><div class="card-body"><h5>'+ listings[i].title +'</h5><h5 class="text-primary">$'+ listings[i].rate +' /night</h5><p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p></div></div></div>'
+    var listingCardString = 
+    '<div class="card-wrap col-lg-3 col-md-12"><div class="card"><img class="card-img-top" src="https://media.architecturaldigest.com/photos/62bdb5fda47d113b36863416/16:9/w_1738,h_977,c_limit/Mod%20Tree%202.jpeg" alt="Card image cap"><div class="card-body"><h5>'+
+    listings[i].title +
+    '</h5><h5 class="text-primary">$' +
+    listings[i].rate +
+    ' /night</h5><p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p></div></div></div>'
     $('#listings').append(listingCardString);
 });
 
@@ -70,58 +110,62 @@ $.each(listings, function(i){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+// Creating markers and their info window.
 function initMap() {
-  const uluru = { lat: 40.7271548, lng: -74.0325778 };
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 13,
-    center: uluru,
+    zoom: 12,
+    center: { lat: 40.731918, lng: -73.995016 },
   });
-  const contentString =
+
+  // Create an info window to share between markers.
+  const infowindow = new google.maps.InfoWindow();
+
+
+  // Create the markers.
+  listings.forEach((singleListing,i) => {
+    var gpsData = [];
+    gpsData.lat = singleListing.lat;
+    gpsData.lng = singleListing.lng;
+
+    const contentString =
     '<div id="content">' +
     '<div id="siteNotice">' +
     "</div>" +
     '<img class="card-img-top" src="https://media.architecturaldigest.com/photos/62bdb5fda47d113b36863416/16:9/w_1738,h_977,c_limit/Mod%20Tree%202.jpeg" alt="Card image cap">' +
     '<h1 id="firstHeading" class="firstHeading">' + 
-    listings[0].title +
-    '</h1>' +
+    singleListing.title +
+    '</h5>' +
+    '<h5 class="text-primary">' +
+    singleListing.rate +
+    ' /night</h5>' +
     '<div id="bodyContent">' +
-    "<p>"+ listings[0].description
+    "<p>"+ 
+    singleListing.description +
     "</p>" +
-    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-    "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-    "(last visited June 22, 2009).</p>" +
+    '<button button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#fullscreenModal">See Details</button>'+
     "</div>" +
     "</div>";
-  const infowindow = new google.maps.InfoWindow({
-    content: contentString,
-    ariaLabel: "Uluru",
-  });
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map,
-    title: "Uluru (Ayers Rock)",
-  });
 
-  marker.addListener("click", () => {
-    infowindow.open({
-      anchor: marker,
+    const marker = new google.maps.Marker({
+      position: gpsData,
       map,
+      title: contentString,
+      optimized: false,
+    });
+
+    // Add a click listener for each marker, and set up the info window.
+    marker.addListener("click", () => {
+      infowindow.close();
+      infowindow.setContent(marker.getTitle());
+      infowindow.open(marker.getMap(), marker);
     });
   });
 }
 
 window.initMap = initMap;
+
+
+
+
 
 
